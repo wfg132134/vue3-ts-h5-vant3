d2ebd3aec24690825074432e3704059dcd8dd6a4
Martin.Liao <@martin.liao>
2023-04-10 18:28:29 +0800

    创建对象式优化封装axios请求

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import http from "@/utils/http";
+import { http } from "@/utils/http";
 
 export function getMydata(){
   return http.httpRequestGet('https://getman.cn/mock/test1',{})
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,35 +1,41 @@
-import axios from "axios"
-const defaultConfig = {
-  timeout: 5000,
-  baseUrl: ''
+import axios, {AxiosRequestConfig} from "axios"
+class Http {
+  public static defaultConfig = {
+    timeout: 5000,
+    baseUrl: ''
+  }
+  constructor() {
+    this.httpInterceptorsRequest();
+    this.httpInterceptorsResponse();
+  }
+
+  private static axiosInstance = axios.create(Http.defaultConfig);
+
+  private httpInterceptorsRequest() {
+    // 请求拦截
+    Http.axiosInstance.interceptors.request.use((config) => {
+      return config;
+    }, (error) => {
+      return Promise.reject(error)
+    })
+  }
+
+  private httpInterceptorsResponse() {
+    // 响应拦截
+    Http.axiosInstance.interceptors.response.use((config) => {
+      return config;
+    }, (error) => {
+      return Promise.reject(error)
+    })
+  }
+
+  public httpRequestGet(url: string, params:AxiosRequestConfig) {
+    return Http.axiosInstance.get(url, params).then((res) => res.data).catch()
+  }
+
+  public httpRequestPost(url: string, params:AxiosRequestConfig) {
+    return Http.axiosInstance.post(url, params).then((res) => res.data).catch()
+  }
 }
-const axiosInstance = axios.create(defaultConfig);
 
-// 请求拦截
-axiosInstance.interceptors.request.use((config)=>{
-  return config;
-},(error)=>{
-  return Promise.reject(error)
-})
-
-// 响应拦截
-axiosInstance.interceptors.response.use((config)=>{
-  return config;
-},(error)=>{
-  return Promise.reject(error)
-})
-
-// 封装请求
-function httpRequestGet(url, params){
-  return axiosInstance.get(url, params).then((res)=> res.data).catch()
-}
-
-// 封装响应
-function httpRequestPost(url, params){
-  return axiosInstance.post(url, params).then((res)=> res.data).catch()
-}
-
-export default {
-  httpRequestGet,
-  httpRequestPost,
-}
+export const http = new Http();

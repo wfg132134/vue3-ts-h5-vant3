7e4bfe02d245d0fab335abfa1e21c4cc05ec7f61
Martin.Liao <@martin.liao>
2023-04-11 16:09:52 +0800

    创建Mock接口，使用promise async await包装异步事务

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,15 @@
 import { http } from "@/utils/http";
+import DB from "@/utils/indexedDB";
+const db = new DB("mytest");
 
+//真实接口
 export function getMydata(){
   return http.httpRequestGet('https://getman.cn/mock/test1',{})
+}
+
+// mock 接口
+export async function fetchMydata(){
+  await db.openStore("room", "id");
+  const res = await db.getItemList("room");
+  return {code: "000", res, success:true};
 }
--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -1,4 +1,3 @@
-
 export default class DB {
   private dbName: string;  // 数据库名称
   private db: any; // 定义数据库对象
@@ -7,26 +6,28 @@ export default class DB {
   }
   public openStore(storeName: string, keyPath: string) {
     const request = window.indexedDB.open(this.dbName, 1)
-    request.onsuccess = (event: any) => {
-      this.db = event.target.result;
-      console.log("数据库打开成功");
-      console.log("event2", event);
-    }
-    request.onerror = (event) => {
-      console.log("数据库打开失败");
-    }
-    request.onupgradeneeded = (event:any) => {
-      console.log("数据库升级成功");
-      this.db = event.target.result;
-      const { result }: any = event.target
-      //创建对象仓库
-      const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
-      //创建这个对象仓库成功的回调
-      store.transaction.oncomplete =(event: any) => {
-        console.log("创建对象仓库成功");
-        
+    return new Promise((resolve,reject)=>{
+      request.onsuccess = (event: any) => {
+        this.db = event.target.result;
+        console.log("数据库打开成功");
+        console.log("event2", event);
+        resolve(true)
       }
-    }
+      request.onerror = (event) => {
+        console.log("数据库打开失败");
+      }
+      request.onupgradeneeded = (event:any) => {
+        console.log("数据库升级成功");
+        this.db = event.target.result;
+        const { result }: any = event.target
+        //创建对象仓库
+        const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
+        //创建这个对象仓库成功的回调
+        store.transaction.oncomplete =(event: any) => {
+          console.log("创建对象仓库成功");
+        }
+      }
+    })
   }
 
   // 新增或者修改数据库数据
@@ -36,38 +37,51 @@ export default class DB {
       ...data,
       updateTime:new Date().getTime()
     });
-    request.onsuccess = (event: any) => {
-      console.log("数据写入成功");
-      console.log("event222", event);
-    }
-    request.onerror = (event:any) => {
-      console.log("数据写入失败");
-    }
+    return new Promise((resolve,reject)=>{
+      request.onsuccess = (event: any) => {
+        console.log("数据写入成功");
+        console.log("event222", event);
+        resolve(event)
+      }
+      request.onerror = (event:any) => {
+        console.log("数据写入失败");
+        reject(event)
+      }
+    })
   }
 
   // 数据库删除数据
   public deleteItem(storeName: string, key: number | string){
     const store = this.db.transaction([storeName],'readwrite').objectStore(storeName);
     const request = store.delete(key);
-    request.onsuccess = (event: any) => {
-      console.log("数据删除成功");
-    }
-    request.onerror = (event:any) => {
-      console.log("数据删除失败");
-    }
+    return new Promise((resolve,reject)=>{
+      request.onsuccess = (event: any) => {
+        resolve(event)
+        console.log("数据删除成功");
+      }
+      
+      request.onerror = (event:any) => {
+        resolve(event)
+        console.log("数据删除失败");
+      }
+    })
   }
 
   // 查询所有的数据
   public getItemList(storeName: string){
     const store = this.db.transaction([storeName]).objectStore(storeName);
     const request = store.getAll();
-    request.onsuccess = (event: any) => {
-      console.log("数据获取全部成功");
-      console.log(event.target.result);
-    }
-    request.onerror = (event:any) => {
-      console.log("数据获取全部失败");
-    }
+    return new Promise((resolve, reject)=>{
+      request.onsuccess = (event: any) => {
+        console.log("数据获取全部成功");
+        console.log(event.target.result);
+        resolve(event.target.result);
+      }
+      request.onerror = (event:any) => {
+        console.log("数据获取全部失败");
+        reject(event)
+      }
+    })
   }
 
     // 查询某一条的数据

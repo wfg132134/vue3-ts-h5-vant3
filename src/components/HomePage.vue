dd3e19994d16e40f94f319224ff2b799185325bb
Martin.Liao <@martin.liao>
2023-04-11 14:58:21 +0800

    crud操作indexDB数据库里的数据

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     home page
-    <el-button type="primary" class="aa"><span>Primary</span></el-button>
+    <el-button type="primary" class="aa" @click="addDataHandel"
+      ><span>添加</span></el-button>
+      <el-button type="primary" class="aa" @click="delDataHandel">删除</el-button>
+      <el-button type="primary" class="aa" @click="getAllDataHandel">获取所有</el-button>
+      <el-button type="primary" class="aa" @click="getOneDataHandel">查询一条</el-button>
     <el-date-picker type="date" placeholder="Pick a day" />
   </div>
 </template>
@@ -15,9 +19,48 @@ export default defineComponent({
   setup() {
     const data = getMydata();
     console.log("data", data);
-    const db =new DB('mytest');
-    db.openStore('room','id');
-    return {};
+    const db = new DB("mytest");
+    db.openStore("room", "id");
+    function addDataHandel() {
+      // 由于打开 IndexedDB是异步的加个定时器避免 db对象还没获取到值导致 报错
+      setTimeout(() => {
+        db.updateItem("room", {
+          nose: "33",
+          eye: "比较大",
+        });
+      // 传入id就可以进行修改的操作
+      //   db.updateItem("room", {
+      //     id: 6,
+      //     nose: "443",
+      //     eye: "比较小",
+      //   });
+      }, 200);
+    }
+    function delDataHandel() {
+      // 由于打开 IndexedDB是异步的加个定时器避免 db对象还没获取到值导致 报错
+      setTimeout(() => {
+        db.deleteItem("room", 8);
+      }, 200);
+    }
+    function getAllDataHandel() {
+      // 由于打开 IndexedDB是异步的加个定时器避免 db对象还没获取到值导致 报错
+      setTimeout(() => {
+        db.getItemList("room");
+      }, 200);
+    }
+
+    function getOneDataHandel() {
+      // 由于打开 IndexedDB是异步的加个定时器避免 db对象还没获取到值导致 报错
+      setTimeout(() => {
+        db.getItemOne("room", 6);
+      }, 200);
+    }
+    return {
+      addDataHandel,
+      delDataHandel,
+      getAllDataHandel,
+      getOneDataHandel,
+    };
   },
 });
 </script>
--- src/utils/indexedDB.ts
+++ src/utils/indexedDB.ts
@@ -1,20 +1,23 @@
 
 export default class DB {
   private dbName: string;  // 数据库名称
+  private db: any; // 定义数据库对象
   constructor(dbName: string) {
     this.dbName = dbName
   }
   public openStore(storeName: string, keyPath: string) {
     const request = window.indexedDB.open(this.dbName, 1)
-    request.onsuccess = (event) => {
+    request.onsuccess = (event: any) => {
+      this.db = event.target.result;
       console.log("数据库打开成功");
       console.log("event2", event);
     }
     request.onerror = (event) => {
       console.log("数据库打开失败");
     }
-    request.onupgradeneeded = (event) => {
+    request.onupgradeneeded = (event:any) => {
       console.log("数据库升级成功");
+      this.db = event.target.result;
       const { result }: any = event.target
       //创建对象仓库
       const store = result.createObjectStore(storeName, { autoIncrement: true, keyPath })
@@ -25,4 +28,58 @@ export default class DB {
       }
     }
   }
+
+  // 新增或者修改数据库数据
+  public updateItem(storeName: string, data: any){
+    const store = this.db.transaction([storeName],'readwrite').objectStore(storeName);
+    const request = store.put({
+      ...data,
+      updateTime:new Date().getTime()
+    });
+    request.onsuccess = (event: any) => {
+      console.log("数据写入成功");
+      console.log("event222", event);
+    }
+    request.onerror = (event:any) => {
+      console.log("数据写入失败");
+    }
+  }
+
+  // 数据库删除数据
+  public deleteItem(storeName: string, key: number | string){
+    const store = this.db.transaction([storeName],'readwrite').objectStore(storeName);
+    const request = store.delete(key);
+    request.onsuccess = (event: any) => {
+      console.log("数据删除成功");
+    }
+    request.onerror = (event:any) => {
+      console.log("数据删除失败");
+    }
+  }
+
+  // 查询所有的数据
+  public getItemList(storeName: string){
+    const store = this.db.transaction([storeName]).objectStore(storeName);
+    const request = store.getAll();
+    request.onsuccess = (event: any) => {
+      console.log("数据获取全部成功");
+      console.log(event.target.result);
+    }
+    request.onerror = (event:any) => {
+      console.log("数据获取全部失败");
+    }
+  }
+
+    // 查询某一条的数据
+    public getItemOne(storeName: string, key: number | string){
+      const store = this.db.transaction([storeName]).objectStore(storeName);
+      const request = store.get(key);
+      request.onsuccess = (event: any) => {
+        console.log("查询这条数据成功");
+        console.log(event.target.result);
+      }
+      request.onerror = (event:any) => {
+        console.log("查询这条数据失败");
+      }
+    }
 }

e52c1d9eb089e5b72f98be94c32fc41f63d2740d
Martin.Liao <@martin.liao>
2023-05-17 15:35:00 +0800

    使用数组reduce函数

--- src/components/DetailPage.vue
+++ src/components/DetailPage.vue
@@ -1,55 +1,59 @@
 <template>
   <div class="grid">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>10</div>
-    <div>11</div>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
+
+// ?计算数组中每个元素出现的次数
+// const fruits =  ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+// const count = fruits.reduce((accumulator, currentValue) => {
+  
+//   accumulator[currentValue] = (accumulator[currentValue] || 0) + 1;
+//   return accumulator;
+// }, {});
+// console.log(count);
+
+// ?拍平嵌套数组
+// const nestedArray = [[1, 2], [3, 4], [5, 6]];
+// const flattenedArray = nestedArray.reduce((accumulator, currentValue) => accumulator.concat(currentValue), []);
+// console.log(flattenedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+// ?按条件分组
+// const people = [
+//   { name: 'Alice', age: 25 },
+//   { name: 'Bob', age: 30 },
+//   { name: 'Charlie', age: 35 },
+//   { name: 'David', age: 25 },
+//   { name: 'Emily', age: 30 }
+// ];
+// const groupedPeople = people.reduce((accumulator, currentValue) => {
+//   const key = currentValue.age;
+//   if (!accumulator[key]) {
+//     accumulator[key] = [];
+//   }
+//   accumulator[key].push(currentValue);
+//   return accumulator;
+// }, {});
+// console.log(groupedPeople);
+
+// ?将多个数组合并为一个对象
+
+// const keys = ['name', 'age', 'gender'];
+// const values = ['Alice', 25, 'female'];
+// const person = keys.reduce((accumulator, currentValue, index) => {
+//     accumulator[currentValue] = values[index];
+//     return accumulator;
+//   }, {});
+// console.log(person); // Output: { name: 'Alice', age: 25, gender: 'female' }
+
+
+
+
 </script>
 
 <style scoped lang="scss">
-.grid {
-  border: 1px solid #999;
-  width: 300px;
-  height: 200px;
-  display: grid;
-  margin: 20px;
-  /* 等同于 grid-template-columns: 1fr 1fr 1fr; */
-	grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 1fr 1fr; /* 指定 2行 */
-  grid-auto-rows: 40px;
 
-  div {
-    background-color: bisque;
-    border-radius: 4px;
-    border: 1px solid #ccc;
-
-    &:nth-of-type(1) {
-      grid-area: 1 / 1 / 2 / 4;
-    }
-
-    &:nth-of-type(2) {
-      grid-row: 2 / 4;
-    }
-
-    &:nth-of-type(3) {
-      grid-column: 2 / span 2; /* span表示占据几行，这里表示从2开始，占据2行，也就是网格线2到4 */
-    }
-
-    &:nth-of-type(6) {
-      grid-column: 1 / span 3;
-    }
-  }
-}
 </style>
